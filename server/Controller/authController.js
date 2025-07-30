0fb16a3b0b39ba530c5f328e50e0a024b49d7958
gitty-333 <g0527677333@gmail.com>
2025-07-30 23:49:23 +0300

    add bcrypt

--- server/Controller/authController.js
+++ server/Controller/authController.js
@@ -1,24 +1,36 @@
 const bcrypt=require("bcrypt")
+const jwt=require("jsonwebtoken")
 const Consultant=require("../models/Consultant")
+//פונקציית כניסה למערכת
 const login=async(req,res)=>{
 const{tz,password}=req.body
-if(!tz||!password){return res.status(400).json({"message":"tz and password are require!"})}
-if(await Consultant.findOne({tz}).exec())
-      return res.status(400).send("tz needs to be uniqe!")
-const consultant = await Consultant.findOne({ tz }).exec();
-if (!consultant) {
-      return res.status(401).json({ message: "Invalid tz or password!" });
-    }
+if(!tz||!password){
+      return res.status(400).json({"message":"All fileds are required!"})}
+const foundConsultant=await Consultant.findOne({tz}).exec()
+if(!foundConsultant){
+      return res.status(401).json({ message: "Unauthorized" });
 }
+const mutch=await bcrypt.compare(password,foundConsultant.password)
+if(!mutch)return res.status(401).json({message:"Unauthorized"})
+res.send("Logged In")
+}
+// פונקציית הרשמה למערכת
 const register=async(req,res)=>{
  const{firstName,lastName,email,password,phone,tz}=req.body
- if(!firstName||!lastName||!email||!password||!phone||!tz){return res.status(400).json({"message":"All fields are required!"})}
-    if(await Consultant.findOne({tz}).exec())
-        return res.status(400).send({message:"tz needs to be uniqe!"})
-  //הצפנת הסיסמא 
+ if(!firstName||!lastName||!email||!password||!phone||!tz){
+      return res.status(400).json({"message":"All fields are required!"})}
+const duplicate=await Consultant.findOne({tz}).exec()
+if(duplicate){
+       return res.status(409).send({message:"duplicate tz!"})
+}
+//הצפנת הסיסמא 
   const hashedPwd=await bcrypt.hash(password,10)
-  const consultantObject={}  
-  //  const consultant=await Consultant.create({firstName,lastName,email,password,phone,tz})
-  //  res.json(consultant)
+  const consultant=await Consultant.create({firstName,lastName,email,phone,tz,password:hashedPwd})
+  if(consultant){
+      return res.status(201).json({message:`new consultant ${consultant.tz} created`})
+  }
+  else{
+      return res.status(400).json({message:"Invalid consultant received"})
+  }
 }
 module.exports={login,register}
